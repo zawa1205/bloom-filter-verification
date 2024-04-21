79eb9de0085c055423a092807e674c4c055ccd89
zawa1205 <aizu.serizawa@gmail.com>
2024-04-21 17:43:19 +0900

    Add error verification

--- apps/pages-only/next.config.js
+++ apps/pages-only/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/base",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ apps/pages-only/src/pages/samples/[id].tsx
@@ -0,0 +1,16 @@
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  const router = useRouter();
+  const { id } = router.query;
+  return (
+    <main className={`${styles.main} ${inter.className}`}>
+      <h1>Pages Router only</h1>
+      <p>{`/samples/${id}`}</p>
+    </main>
+  );
+}
