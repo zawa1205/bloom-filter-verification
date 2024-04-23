85cfa745da9a068589bec083978cf9f97306bc3b
zawa1205 <aizu.serizawa@gmail.com>
2024-04-23 23:25:03 +0900

    Fix next.config

--- apps/pages-and-app/next.config.js
+++ apps/pages-and-app/next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: "/base",
+  experimental: {
+    // ここを false にすると Bloom Filter を使わないようにできる
+    // clientRouterFilter: false,
+  },
+
+  redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/base",
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- apps/pages-only/next.config.js
+++ apps/pages-only/next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: "/base",
+  redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/base",
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
